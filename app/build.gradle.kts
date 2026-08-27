cbbcb75bc23f5b4039d9475d7e7678c93d2e4c2a
Magna-Create <53941598+Magna-Create@users.noreply.github.com>
2026-08-27 03:19:25 +0100

    Remove unused adaptive layout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -132,7 +132,6 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material3.adaptive:adaptive:1.3.0")
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
